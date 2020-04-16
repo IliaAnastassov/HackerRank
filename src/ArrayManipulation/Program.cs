71648c14188a02010a9d5f4286f998d099d867d1
Ilia Anastassov <ilia.anastassov@gmail.com>
2020-04-16 16:02:05 +0300

    Solve problems

--- src/ArrayManipulation/Program.cs
+++ src/ArrayManipulation/Program.cs
@@ -15,22 +15,23 @@ namespace ArrayManipulation
 
             for (int i = 0; i < operationsCount; i++)
             {
-                queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+                queries[i] = Array.ConvertAll(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), queriesTemp => Convert.ToInt32(queriesTemp));
             }
 
-            long result = ArrayManipulation(arraySize, queries);
+            long result = ArrayManipulationOptimized(arraySize, queries);
         }
 
-        private static long ArrayManipulation(int arraySize, int[][] queries)
+        /// <summary>
+        /// 10 2
+        /// 1 5 3
+        /// 4 8 7
+        ///  1   2   3   4   5   6   7   8   9   10
+        ///[ 0   0   0   0   0   0   0   0   0   0 ]
+        ///[ 3   3   3   3   3   0   0   0   0   0 ]
+        ///[ 3   3   3   10  10  7   7   7   0   0 ]
+        /// </summary>
+        private static long ArrayManipulationBruteForce(int arraySize, int[][] queries)
         {
-            // a b k
-            // 1 5 3
-            // 4 8 7
-            //  1   2   3   4   5   6   7   8   9   10
-            //[ 0   0   0   0   0   0   0   0   0   0 ]
-            //[ 3   3   3   3   3   0   0   0   0   0 ]
-            //[ 3   3   3   10  10  7   7   7   0   0 ]
-
             var numbers = new long[arraySize];
 
             int firstIndex, lastIndex, valueToAdd;
@@ -47,6 +48,43 @@ namespace ArrayManipulation
             }
 
             return numbers.Max();
+        }
+
+        /// <summary>
+        /// 10 3
+        /// 1 5 3
+        /// 4 8 7
+        /// 5 10 4
+        ///  1   2   3   4   5   6   7   8   9   10 (11)
+        ///[ 0   0   0   0   0   0   0   0   0   0   0 ]
+        ///[ 3   0   0   0   0  -3   0   0   0   0   0 ]
+        ///[ 3   0   0   7   0  -3   0   0  -7   0   0 ]
+        ///[ 3   0   0   7   4  -3   0   0  -7   0  -4 ]
+        /// </summary>
+        private static long ArrayManipulationOptimized(int arraySize, int[][] queries)
+        {
+            var numbers = new long[arraySize + 1];
+
+            int firstIndex, lastIndex, valueToAdd;
+            foreach (var query in queries)
+            {
+                firstIndex = query[0] - 1;
+                lastIndex = query[1];
+                valueToAdd = query[2];
+
+                numbers[firstIndex] += valueToAdd;
+                numbers[lastIndex] -= valueToAdd;
+            }
+
+            var maxSum = 0L;
+            var currentSum = 0L;
+            foreach (var number in numbers)
+            {
+                currentSum += number;
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+
+            return maxSum;
         }
     }
 }
---
+++ src/MinimumSwaps/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MinimumSwaps
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            var result = GetMinimumSwapsOptimized(numbers);
+        }
+
+        /// <summary>
+        /// 7 1 3 2 4 5 6
+        /// </summary>
+        private static int GetMinimumSwaps(int[] numbers)
+        {
+            var swapsCount = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var localMin = numbers[i];
+                var localMinIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < localMin)
+                    {
+                        localMin = numbers[j];
+                        localMinIndex = j;
+                    }
+                }
+
+                if (localMinIndex != i)
+                {
+                    Swap(numbers, i, localMinIndex);
+                    swapsCount++;
+                }
+            }
+
+            return swapsCount;
+        }
+
+        /// <summary>
+        /// 7 1 3 2 4 5 6
+        /// </summary>
+        private static int GetMinimumSwapsOptimized(int[] numbers)
+        {
+            var swapsCount = 0;
+
+            var i = 0;
+            while (i < numbers.Length - 1)
+            {
+                while (numbers[i] == i + 1 && i < numbers.Length - 1)
+                {
+                    i++;
+                }
+
+                if (i < numbers.Length - 1)
+                {
+                    Swap(numbers, i, numbers[i] - 1);
+                    swapsCount++;
+                }
+            }
+
+            return swapsCount;
+        }
+
+        private static void Swap(int[] array, int index1, int index2)
+        {
+            array[index1] ^= array[index2];
+            array[index2] ^= array[index1];
+            array[index1] ^= array[index2];
+        }
+    }
+}
