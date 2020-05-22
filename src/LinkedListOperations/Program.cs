310a582d7a45e74c56b0ec63489ac5f7e0052c49
Ilia Anastassov <ilia.anastassov@gmail.com>
2020-05-22 13:15:04 +0300

    Add solution

--- src/LinkedListOperations/Program.cs
+++ src/LinkedListOperations/Program.cs
@@ -14,7 +14,7 @@ namespace LinkedListOperations
             linkedList.InsertNode(4);
             linkedList.InsertNode(5);
 
-            var head = Reverse(linkedList.Head);
+            var head = ReverseRecursive(linkedList.Head);
             PrintSinglyLinkedList(head, " ");
         }
 
@@ -40,15 +40,6 @@ namespace LinkedListOperations
             return head;
         }
 
-        public static void ReversePrint(SinglyLinkedListNode head)
-        {
-            if (head != null)
-            {
-                ReversePrint(head.next);
-                Console.WriteLine(head.data);
-            }
-        }
-
         public static SinglyLinkedListNode Reverse(SinglyLinkedListNode head)
         {
             var current = head;
@@ -57,8 +48,11 @@ namespace LinkedListOperations
 
             while (current != null)
             {
+                // Store the link for the next node and link the current node to the previous node
                 next = current.next;
                 current.next = previous;
+
+                // Set previous node to current and current to next
                 previous = current;
                 current = next;
             }
@@ -66,18 +60,50 @@ namespace LinkedListOperations
             return previous;
         }
 
+        public static SinglyLinkedListNode ReverseRecursive(SinglyLinkedListNode head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            // last node or only one node
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            var newHeadNode = ReverseRecursive(head.next);
+
+            // change references for middle chain
+            head.next.next = head;
+            head.next = null;
+
+            // send back new head
+            // node in every recursion
+            return newHeadNode;
+        }
+
         public static void PrintSinglyLinkedList(SinglyLinkedListNode node, string separator)
         {
             while (node != null)
             {
                 Console.Write(node.data);
-
                 node = node.next;
 
                 if (node != null)
                 {
                     Console.Write(separator);
                 }
+            }
+        }
+
+        public static void PrintReverse(SinglyLinkedListNode node)
+        {
+            if (node != null)
+            {
+                PrintReverse(node.next);
+                Console.Write($"{node.data} ");
             }
         }
     }
